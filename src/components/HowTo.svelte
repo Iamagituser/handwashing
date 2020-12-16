5681f101a0bc79e90c2e3826473128686ecc5c8b
Davinder brar <davinderbrar@192-168-1-103.tpgi.com.au>
2020-12-16 21:27:48 +1030

    wtitten html and css for components

---
+++ src/components/HowTo.svelte
@@ -0,0 +1,13 @@
+<style>
+    img {
+        max-width: 100%;
+    }
+    .grid {
+        grid-template-columns: auto;
+        justify-content: center;
+    }
+</style>
+
+<div class="grid">
+    <img src="howto.png" alt="how to wash your hands">
+</div>
---
+++ src/components/ProgressBar.svelte
@@ -0,0 +1,24 @@
+<style>
+    .progress-bar {
+        border : 1px solid rgb(163, 162, 162);
+        margin: 20px 0px;
+    }
+    .progress {
+        width:20%;
+        background-color: rgb(62, 214, 133);
+        height: 40px;
+    }
+    button {
+        width: 100%;
+        background-color:rgb(236, 10, 115);
+        color :white;
+        margin: 10px 0;
+    }
+</style>
+
+<h1>Seconds Left: </h1>
+
+<div class = "progress-bar">
+    <div class="progress"><span class="sr-only">%</span></div>
+</div>
+<button>Start</button>
